a6b0fa5e77131db83ed97645d69778be2eba5aa4
Fatih Es <54971670+fatihes1@users.noreply.github.com>
2022-05-06 17:56:23 +0300

    end of welcome page of getir.com

--- src/components/ui/Menu.vue
+++ src/components/ui/Menu.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>{{ menu.title }}</h1>
+  <nav class="grid gap-y-3">
+      <h6 class="text-lg text-brand-color">{{ data.title }}</h6>
+      <nav>
+          <ul class="grid gap-y-3">
+              <li v-for="item in data.items" :key="item">
+                  <a class="text-sm font-thin text-gray-600 hover:text-primary-brand-color/90" :href=item.url>{{ item.subTitle }}</a>
+              </li>
+          </ul>
+      </nav>
+  </nav>
 </template>
 
 <script>
-props : ['menuData']
-export default {
 
+export default {
+    props : ['data']
 }
 </script>
 
